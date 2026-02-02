79071ac38be6191bbc2c211afb71a7c76e6e4d15
Drew-Andersen <andrewtandersen@gmail.com>
2026-02-02 11:56:16 -0500

    Created bert-qa.py file with notes on what I need to add

---
+++ src/bert_qa.py
@@ -0,0 +1,10 @@
+"""
+BERT-based Question Andwering using Hugging Face Transformers.
+This module answers questions strickly based on the provided documents.
+"""
+
+# TODO: Import libaries
+# TODO: Load document text
+# TODO: Initialize question-answering pipelines
+# TODO: Answer user question
+# TODO: Print formatted output
